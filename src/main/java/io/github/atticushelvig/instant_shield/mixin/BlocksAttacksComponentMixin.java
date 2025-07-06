771b82fd67e81eb373b12979200b532cf01690e5
Atticus Helvig <atticushelvig@gmail.com>
2025-07-06 14:48:29 -0700

    feat: backport to 1.21.4

--- src/main/java/io/github/atticushelvig/instant_shield/mixin/BlocksAttacksComponentMixin.java
+++ src/main/java/io/github/atticushelvig/instant_shield/mixin/BlocksAttacksComponentMixin.java
@@ -1,17 +1,15 @@
 package io.github.atticushelvig.instant_shield.mixin;
 
-import net.minecraft.component.type.BlocksAttacksComponent;
+import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(BlocksAttacksComponent.class)
+@Mixin(LivingEntity.class)
 public abstract class BlocksAttacksComponentMixin {
 
-    @Inject(method = "getBlockDelayTicks", at = @At("RETURN"), cancellable = true)
-    private void modifyBlockDelay(CallbackInfoReturnable<Integer> cir) {
-        cir.setReturnValue(0);
-        cir.cancel();
+    @ModifyConstant(method = "getBlockingItem", constant = @Constant(intValue = 5))
+    private int modifyBlockDelay(int constant) {
+        return 0;
     }
 }
