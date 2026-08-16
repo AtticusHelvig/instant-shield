cf5f5a273065a63706a47e5a9c48ff21daaa00af
Atticus Helvig <atticushelvig@gmail.com>
2026-08-15 17:51:46 -0700

    chore: migrate to mojang mappings

--- src/main/java/io/github/atticushelvig/instant_shield/mixin/BlocksAttacksComponentMixin.java
+++ src/main/java/io/github/atticushelvig/instant_shield/mixin/BlocksAttacksComponentMixin.java
@@ -1,15 +1,15 @@
 package io.github.atticushelvig.instant_shield.mixin;
 
-import net.minecraft.component.type.BlocksAttacksComponent;
+import net.minecraft.world.item.component.BlocksAttacks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(BlocksAttacksComponent.class)
+@Mixin(BlocksAttacks.class)
 public abstract class BlocksAttacksComponentMixin {
 
-    @Inject(method = "getBlockDelayTicks", at = @At("RETURN"), cancellable = true)
+    @Inject(method = "blockDelayTicks", at = @At("RETURN"), cancellable = true)
     private void modifyBlockDelay(CallbackInfoReturnable<Integer> cir) {
         cir.setReturnValue(0);
         cir.cancel();
